709b22938a5119211e6c0e2cabefad0d073e7935
Eva <evainga>
2017-05-17 21:34:03 +0200

    add first step of Roman Numbers Kata

---
+++ src/main/RomanCharToInt.java
@@ -0,0 +1,28 @@
+package main;
+
+public class RomanCharToInt {
+
+	public int romanCharToInt(char romanChar) {
+
+		switch (romanChar) {
+		case 'I':
+			return 1;
+		case 'V':
+			return 5;
+		case 'X':
+			return 10;
+		case 'L':
+			return 50;
+		case 'C':
+			return 100;
+		case 'D':
+			return 500;
+		case 'M':
+			return 1000;
+
+		}
+		System.out.println("This is not a valid Roman Number. Please enter a valid Number.");
+		return 0;
+	}
+
+}
---
+++ src/main/RomanCharToIntTest.java
@@ -0,0 +1,33 @@
+package main;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class RomanCharToIntTest {
+
+	RomanCharToInt intNumber = new RomanCharToInt();
+
+	@DataProvider(name = "romanCharToInt")
+	public Object[][] createData1() {
+		return new Object[][] {
+				{ 'I', 1 },
+				{ 'V', 5 },
+				{ 'X', 10 },
+				{ 'L', 50 },
+				{ 'C', 100 },
+				{ 'D', 500 },
+				{ 'M', 1000 },
+				{ 'A', 0 },
+				{ 'Z', 0 },
+
+		};
+	}
+
+	@Test(dataProvider = "romanCharToInt")
+	public void romanCharToInt(char romanChar, int decimal) {
+		assertThat(intNumber.romanCharToInt(romanChar), is(decimal));
+	}
+}
