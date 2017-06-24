5c3c19e1b2d3d0fd733f9bfbf486e2706661cf48
Eva <evainga>
2017-06-24 23:00:23 +0200

    improve TxtReader to more flexiblility and delete old code

--- src/main/java/de/regex/TxtReader.java
+++ src/main/java/de/regex/TxtReader.java
@@ -2,20 +2,30 @@ package de.regex;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.io.FileUtils;
 
 public class TxtReader {
 
-	String filepath = "/guest-and-host.txt";
-
-	public String txtToString(String filepath) throws IOException {
-		this.filepath = filepath;
-		String filename = TxtReader.class.getResource(filepath).getFile();
+	public String txtToString(String filePath) throws IOException {
+		String filename = TxtReader.class.getResource(filePath).getFile();
 		return FileUtils.readFileToString(new File(filename), "UTF-8");
 	}
 
-	// public String getfilePath(){
-	// return "/guest-and-host.txt";
-	// }
+	public List<String> findRegExMatchingStringsInText(String regEx, String filePath) throws IOException {
+
+		List<String> matchList = new ArrayList<>();
+		Pattern pattern = Pattern.compile(regEx);
+		Matcher matcher = pattern.matcher(txtToString(filePath));
+
+		while (matcher.find()) {
+			matchList.add(matcher.group());
+		}
+		return matchList;
+	}
+
 }
--- src/test/java/de/regex/TxtReaderTest.java
+++ src/test/java/de/regex/TxtReaderTest.java
@@ -1,7 +1,11 @@
 package de.regex;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.startsWith;
 
 import java.io.IOException;
@@ -10,11 +14,92 @@ import org.testng.annotations.Test;
 
 public class TxtReaderTest {
 	private TxtReader fileReader = new TxtReader();
+	private String filePath = "/guest-and-host.txt";
 
 	@Test
 	public void txtToString() throws IOException {
-		assertThat(fileReader.txtToString("/guest-and-host.txt"), startsWith("4. The Master ascended the high seat."));
-		assertThat(fileReader.txtToString("/guest-and-host.txt"),
+		assertThat(fileReader.txtToString(filePath), startsWith("4. The Master ascended the high seat."));
+		assertThat(fileReader.txtToString(filePath),
 				endsWith("Rinzai‘s (Linji) host and guest, ask the head monks of the two halls."));
 	}
+
+	@Test
+	public void findAllWords() throws IOException {
+		String allWords = "[\\p{L}‘]+";
+		assertThat(fileReader.findRegExMatchingStringsInText(allWords, filePath), hasSize(168));
+		assertThat(fileReader.findRegExMatchingStringsInText(allWords, filePath), hasItem("Heshang"));
+		assertThat(fileReader.findRegExMatchingStringsInText(allWords, filePath), hasItem("you‘d"));
+		assertThat(fileReader.findRegExMatchingStringsInText(allWords, filePath), hasItem("Rinzai‘s"));
+		assertThat(fileReader.findRegExMatchingStringsInText(allWords, filePath), hasItem("Oshô"));
+	}
+
+	@Test
+	public void startsWithUpperCaseTest() throws IOException {
+		String startsWithUpperCase = "[\\p{Lu}]([\\p{L}‘])*";
+		assertThat(fileReader.findRegExMatchingStringsInText(startsWithUpperCase, filePath), hasItem("Heshang"));
+		assertThat(fileReader.findRegExMatchingStringsInText(startsWithUpperCase, filePath), hasItem("Rinzai‘s"));
+		assertThat(fileReader.findRegExMatchingStringsInText(startsWithUpperCase, filePath), hasItem("Oshô"));
+		assertThat(fileReader.findRegExMatchingStringsInText(startsWithUpperCase, filePath), not(hasItem("the")));
+		assertThat(fileReader.findRegExMatchingStringsInText(startsWithUpperCase, filePath), hasSize(43));
+	}
+
+	@Test
+	public void startsWithLowerCaseMTest() throws IOException {
+		String startsWithLowerCaseM = "\\bm([\\p{L}‘])*";
+		assertThat(fileReader.findRegExMatchingStringsInText(startsWithLowerCaseM, filePath), not(hasItem("Master")));
+		assertThat(fileReader.findRegExMatchingStringsInText(startsWithLowerCaseM, filePath),
+				not(hasItem("mmediately")));
+		assertThat(fileReader.findRegExMatchingStringsInText(startsWithLowerCaseM, filePath), hasItem("me"));
+		assertThat(fileReader.findRegExMatchingStringsInText(startsWithLowerCaseM, filePath), hasItem("monks"));
+		assertThat(fileReader.findRegExMatchingStringsInText(startsWithLowerCaseM, filePath), hasSize(16));
+	}
+
+	@Test
+	public void hasFourLettersTest() throws IOException {
+		String hasFourLetters = "\\b[\\p{L}‘]{4}\\b";
+		assertThat(fileReader.findRegExMatchingStringsInText(hasFourLetters, filePath), not(hasItem("Mast")));
+		assertThat(fileReader.findRegExMatchingStringsInText(hasFourLetters, filePath), hasItem("high"));
+		assertThat(fileReader.findRegExMatchingStringsInText(hasFourLetters, filePath), hasItem("came"));
+		assertThat(fileReader.findRegExMatchingStringsInText(hasFourLetters, filePath), hasItem("monk"));
+		assertThat(fileReader.findRegExMatchingStringsInText(hasFourLetters, filePath), not(hasItem("mday,")));
+		assertThat(fileReader.findRegExMatchingStringsInText(hasFourLetters, filePath), hasItem("Oshô"));
+		// assertThat(fileReader.findRegExMatchingStringsInText(hasFourLetters,
+		// filePath), hasItem("It‘s"));
+		// assertThat(fileReader.findRegExMatchingStringsInText(hasFourLetters,
+		// filePath), not(hasItem("you‘")));
+		// to be fixed
+		assertThat(fileReader.findRegExMatchingStringsInText(hasFourLetters, filePath), hasSize(41));
+	}
+
+	@Test
+	public void endsWithEdTest() throws IOException {
+		String endsWithEd = "([\\p{L}‘])*ed\\b";
+		assertThat(fileReader.findRegExMatchingStringsInText(endsWithEd, filePath), hasItem("ascended"));
+		assertThat(fileReader.findRegExMatchingStringsInText(endsWithEd, filePath), not(hasItem("immed")));
+		assertThat(fileReader.findRegExMatchingStringsInText(endsWithEd, filePath), hasSize(14));
+	}
+
+	@Test
+	public void hasALetterTest() throws IOException {
+		String hasALetter = "([\\p{L}‘])*a([\\p{L}‘])*";
+		assertThat(fileReader.findRegExMatchingStringsInText(hasALetter, filePath), hasItem("Heshang"));
+		assertThat(fileReader.findRegExMatchingStringsInText(hasALetter, filePath), hasItem("a"));
+		assertThat(fileReader.findRegExMatchingStringsInText(hasALetter, filePath), not(hasItem("Another")));
+		assertThat(fileReader.findRegExMatchingStringsInText(hasALetter, filePath), hasSize(62));
+	}
+
+	@Test
+	public void getNumericsTest() throws IOException {
+		String numerics = "\\d";
+		assertThat(fileReader.findRegExMatchingStringsInText(numerics, filePath), contains("4"));
+		assertThat(fileReader.findRegExMatchingStringsInText(numerics, filePath), hasSize(1));
+	}
+
+	@Test
+	public void hasCommaTest() throws IOException {
+		String hasComma = ", ";
+		assertThat(fileReader.findRegExMatchingStringsInText(hasComma, filePath), hasItem(", "));
+		assertThat(fileReader.findRegExMatchingStringsInText(hasComma, filePath), hasSize(15));
+	}
+
 }
