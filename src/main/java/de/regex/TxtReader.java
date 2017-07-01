5a628928ac628f49d2b66fc8ce34c8ae643564fa
Eva <evainga>
2017-07-01 17:35:41 +0200

    delete unused learning code

--- src/main/java/de/regex/TxtReader.java
+++ src/main/java/de/regex/TxtReader.java
@@ -2,14 +2,8 @@ package de.regex;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-
-import com.google.common.collect.Lists;
 
 public class TxtReader {
 
@@ -18,10 +12,6 @@ public class TxtReader {
 		List<String> matchList = new ArrayList<>();
 		Pattern pattern = Pattern.compile(regEx);
 		Matcher matcher = pattern.matcher(text);
-
-		Predicate<? super Object> predicate = Objects::nonNull;
-		Function<? super Object, ? extends Object> function = Function.identity();
-		Lists.newArrayList().stream().filter(predicate).map(function).collect(Collectors.toList());
 
 		while (matcher.find()) {
 			matchList.add(matcher.group());
