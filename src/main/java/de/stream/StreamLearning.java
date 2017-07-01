eb2fdbfb85d34e62a720de97c9437665b1fb289d
Eva <evainga>
2017-07-01 18:30:15 +0200

    add StreamLearningTests

--- src/main/java/de/stream/StreamLearning.java
+++ src/main/java/de/stream/StreamLearning.java
@@ -12,28 +12,17 @@ public class StreamLearning {
 				.collect(Collectors.toList());
 	}
 
-	public static void main(String[] args) {
-
-		List<String> myList = Arrays.asList("a1", "a2", "b1", "c2", "c1");
-
-		myList.stream()
-				.filter(item -> item.startsWith("a"))
-				.collect(Collectors.toList())
-				.forEach(System.out::println);
-
-		String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-		List<Character> charAbcList = abc
+	public List<Character> streamAlphabet() {
+		return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 				.chars()
 				.mapToObj(character -> (char) character)
 				.collect(Collectors.toList());
-
-		System.out.println(charAbcList);
-
-		charAbcList.stream()
-				.map(character -> character.toString() + "A")
-				.collect(Collectors.toList())
-				.forEach(System.out::print);
 	}
 
+	public List<String> addAToAlphabetTest() {
+		return streamAlphabet()
+				.stream()
+				.map(character -> character.toString() + "A")
+				.collect(Collectors.toList());
+	}
 }
--- src/test/java/de/stream/StreamLearningTest.java
+++ src/test/java/de/stream/StreamLearningTest.java
@@ -1,6 +1,7 @@
 package de.stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasSize;
 
 import org.testng.annotations.Test;
@@ -9,7 +10,22 @@ public class StreamLearningTest {
 	private StreamLearning learning = new StreamLearning();
 
 	@Test
-	public void playAround() throws Exception {
+	public void playAroundTest() throws Exception {
 		assertThat(learning.playAround(), hasSize(1));
 	}
+
+	@Test
+	public void streamAlphabetTest() throws Exception {
+		assertThat(learning.streamAlphabet(), hasSize(26));
+		assertThat(learning.streamAlphabet(), hasItem('A'));
+		assertThat(learning.streamAlphabet(), hasItem('Z'));
+	}
+
+	@Test
+	public void addAToAlphabetTest() throws Exception {
+		assertThat(learning.addAToAlphabetTest(), hasSize(26));
+		assertThat(learning.addAToAlphabetTest(), hasItem("AA"));
+		assertThat(learning.addAToAlphabetTest(), hasItem("ZA"));
+	}
+
 }
